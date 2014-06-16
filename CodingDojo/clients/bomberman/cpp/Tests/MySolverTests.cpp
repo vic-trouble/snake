54fb047b376318bd3fd1c64d38223e9f15b31b70
Vic Trouble <vic.trouble@gmail.com>
2014-06-16 15:54:53 +0300

    some more linking config

--- CodingDojo/clients/bomberman/cpp/Tests/MySolverTests.cpp
+++ CodingDojo/clients/bomberman/cpp/Tests/MySolverTests.cpp
@@ -1,7 +1,13 @@
 #include "stdafx.h"
 #include <gtest/gtest.h>
 
+#include "BomberLib/utils/RandomDice.h"
+#include "MySolver/MySolver.h"
+
 TEST(MySolver, EpicFail)
 {
+	RandomDice d;
+	MySolver solver(&d);
+
 	EXPECT_EQ(5, 4 * 4);
 }
